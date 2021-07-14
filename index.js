2880950364064d86a9db8909fe64a87f064e8e9b
Edison Cai <edisoncai2001@gmail.com>
2021-07-14 12:59:40 +0100

    trying out different types of variables, objects and arrays

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 // Hello
 console.log('Hello World');
 
+// Declaring single variable
 let name = 'Ed';
+
+// Declaring multiple variables
+let firstName = 'Johnny', lastName = 'Depp', email = 'johnnydepp@gmail.com';
+
+// Declaring const
+const interestRate = 0.3;
+
+let age = 30; // Number literal
+let isApproved = true; // Boolean literal
+let preference = undefined;
+let selectedColour = null;
+
+// Creating an object
+let person = {
+    name: 'John',
+    age: 30
+};
+
+person.name = 'Bruh Manix'
+
+// Bracket notation can take variable values
+let selection = 'name'
+person[selection] = 'Brah Bruh'
+
+// Trying out arrays
+// You can have elements of different types in one array
+let selectColours = ['red', 'blue'];
+selectColours[2] = 'green'
+selectColours[3] = 1
+
 console.log(name)
+console.log(person.name)
+console.log(selectColours)
